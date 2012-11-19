7ca29ab6496fd3bfbc7747a9a6824a796c47e109
sithlord48 <sithlord48@6078b701-62e0-4143-b5eb-4703695a1e3f>
2012-11-19 16:33:34 +0000

    + SlotSelect Demo.

--- FF7tk/mainwindow.h
+++ FF7tk/mainwindow.h
@@ -20,12 +20,18 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+
+//Include FF7Save for slot preview.
+
+//#include"static_data/FF7Save.h"
+
 #include "widgets/DialogPreview.h"
 #include "widgets/MateriaEditor.h"
 #include "widgets/CharEditor.h"
 #include "widgets/ChocoboEditor.h"
 #include "widgets/ItemList.h"
 #include "widgets/MetadataCreator.h"
+#include "widgets/SlotSelect.h"
 
 namespace Ui {
     class MainWindow;
@@ -47,15 +53,18 @@ private slots:
     void on_pushButton_clicked();
     void on_checkBox_toggled(bool checked);
     void on_btn_showmetaData_clicked();
+    void on_btn_slotSelect_clicked();
 
 private:
 
     Ui::MainWindow *ui;
+    FF7Save *ff7save;
     DialogPreview *dialog_preview;
     MateriaEditor *materia_editor;
     CharEditor *char_editor;
     ChocoboEditor *choco_editor;
     ItemList *item_list;
+    SlotSelect *slotSelect;
 };
 
 #endif // MAINWINDOW_H
