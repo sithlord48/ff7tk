836d85f405d0089ec96bcef2bf31fbb1ac6a264c
Chris Rizzitello <sithlord48@gmail.com>
2022-01-24 10:41:46 -0500

    Cleaner Statgain

--- src/data/FF7Char.cpp
+++ src/data/FF7Char.cpp
@@ -1,5 +1,5 @@
 /****************************************************************************/
-//    copyright 2012 - 2020 Chris Rizzitello <sithlord48@gmail.com>         //
+//    copyright 2012 - 2022 Chris Rizzitello <sithlord48@gmail.com>         //
 //                                                                          //
 //    This file is part of FF7tk                                            //
 //                                                                          //
@@ -169,22 +169,18 @@ int FF7Char::statGain(int who, int stat, int stat_amount, int current_lvl, int n
 {
     who = std::clamp(who, 0, 10);
     stat = std::clamp(stat, 0, 7);
-    stat_amount = std::clamp(stat_amount, 0, 255);
+    stat_amount = std::clamp(stat_amount, 1, 255); // the stat must be at least one
     current_lvl = std::clamp(current_lvl, 0, 99);
     next_lvl = std::clamp(next_lvl, 0, 99);
     int randomNumber = QRandomGenerator::system()->bounded(1,9);
     int gain = 0; //return this
-    int diff = 0; //holds our dif
-    int lvl_bracket = 0; //track what bracket in the gradent/base were looking at.
+    int diff = 0; //holds our diff
     //0:str; 1:vit; 2:mag; 3:spi; 4:dex; 5:luck; 6:hp; 7:mp
     int baseline_stat = 0; // holds our baseline_stat calculation.
-    //first find out the level bracket
-    if (stat_amount == 0) {
-        stat_amount = 1;
-    }
-    if (next_lvl >= 2 && next_lvl <= 11) {
-        lvl_bracket = 0;
-    } else if (next_lvl >= 12 && next_lvl <= 21) {
+    bool levelUp = (current_lvl < next_lvl);
+
+    int lvl_bracket = 0;
+    if (next_lvl >= 12 && next_lvl <= 21) {
         lvl_bracket = 1;
     } else if (next_lvl >= 22 && next_lvl <= 31) {
         lvl_bracket = 2;
@@ -199,27 +195,24 @@ int FF7Char::statGain(int who, int stat, int stat_amount, int current_lvl, int n
     } else if (next_lvl >= 82 && next_lvl <= 99) {
         lvl_bracket = 7;
     }
-    //calculate the baseline to use.
+
+    //Get the baseline
     if (stat < 5) {
         //calculating str,vit,mag,spi or dex
         int grade = stat_grade(who, stat);
         baseline_stat = stat_base(grade, lvl_bracket) + ((stat_gradent(grade, lvl_bracket) * next_lvl) / 100);
     } else if (stat == 5) {
-        baseline_stat = luck_base(who, lvl_bracket) + ((luck_gradent(who, lvl_bracket)) / 100);   //
+        baseline_stat = luck_base(who, lvl_bracket) + ((luck_gradent(who, lvl_bracket)) / 100);
     } else if (stat == 6) {
         baseline_stat = hp_base(who, lvl_bracket) + (next_lvl - 1) * hp_gradent(who, lvl_bracket);
     } else if (stat == 7) {
         baseline_stat = mp_base(who, lvl_bracket) + ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10);
     }
+
     //now calc the diff, so we can send back a gain based on type
     if (stat < 6) {
         //str, vit,mag, spr,dex or luck all calculated the same
-        //Vegeta_Ss4 lv down mod
-        if (current_lvl < next_lvl) {
-            diff = randomNumber + (baseline_stat - stat_amount);   //is lv up
-        } else {
-            diff = randomNumber - baseline_stat + stat_amount;   //lv down
-        }
+        diff = (levelUp) ? randomNumber + (baseline_stat - stat_amount) : randomNumber - baseline_stat + stat_amount;
         if (diff < 4) {
             gain = 0;
         } else if (diff < 7) {
@@ -232,69 +225,23 @@ int FF7Char::statGain(int who, int stat, int stat_amount, int current_lvl, int n
     } else if (stat == 6) {
         // Base HP Gain
         //Vegeta_Ss4 lv down mod
-        if (current_lvl < next_lvl) {
+        if (levelUp) {
             diff = randomNumber + (100 * baseline_stat / stat_amount) - 100;   //is lv up
         } else if (baseline_stat != 0) {
             diff = randomNumber + (100 * stat_amount / baseline_stat) - 100;   //lv down
         }
-        if (diff == 0) {
-            gain = int(hp_gradent(who, lvl_bracket) * 0.40);
-        } else if (diff == 1) {
-            gain = int(hp_gradent(who, lvl_bracket) * .50);
-        } else if (diff == 2) {
-            gain = int(hp_gradent(who, lvl_bracket) * .50);
-        } else if (diff == 3) {
-            gain = int(hp_gradent(who, lvl_bracket) * .60);
-        } else if (diff == 4) {
-            gain = int(hp_gradent(who, lvl_bracket) * .70);
-        } else if (diff == 5) {
-            gain = int(hp_gradent(who, lvl_bracket) * .80);
-        } else if (diff == 6) {
-            gain = int(hp_gradent(who, lvl_bracket) * .90);
-        } else if (diff == 7) {
-            gain = int(hp_gradent(who, lvl_bracket) * 1.00);
-        } else if (diff == 8) {
-            gain = int(hp_gradent(who, lvl_bracket) * 1.10);
-        } else if (diff == 9) {
-            gain = int(hp_gradent(who, lvl_bracket) * 1.20);
-        } else if (diff == 10) {
-            gain = int(hp_gradent(who, lvl_bracket) * 1.30);
-        } else if (diff >= 11) {
-            gain = int(hp_gradent(who, lvl_bracket) * 1.50);
-        }
+        diff = std::clamp(diff, 0 , 11);
+        gain = int(hp_gradent(who, lvl_bracket) * d->_hp_diff_modifier.at(diff));
     } else if (stat == 7) {
         // Base MP Gain
         //Vegeta_Ss4 lv down mod
-        if (current_lvl < next_lvl) {
+        if (levelUp) {
             diff = randomNumber + (100 * baseline_stat / stat_amount) - 100;   //is lv up
         } else if (baseline_stat != 0) {
             diff = randomNumber + (100 * stat_amount / baseline_stat) - 100;   //lv down
         }
-        if (diff == 0) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 0.20);
-        } else if (diff == 1) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .30);
-        } else if (diff == 2) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .30);
-        } else if (diff == 3) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .50);
-        } else if (diff == 4) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .70);
-        } else if (diff == 5) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .80);
-        } else if (diff == 6) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * .90);
-        } else if (diff == 7) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 1.00);
-        } else if (diff == 8) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 1.10);
-        } else if (diff == 9) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 1.20);
-        } else if (diff == 10) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 1.40);
-        } else if (diff >= 11) {
-            gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * 1.60);
-        }
+        diff = std::clamp(diff, 0 , 11);
+        gain = int(((next_lvl * mp_gradent(who, lvl_bracket) / 10) - ((next_lvl - 1) * mp_gradent(who, lvl_bracket) / 10)) * d->_mp_diff_modifier.at(diff));
     }
     return gain;
 }
