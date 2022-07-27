13c4d12062de832e3f496ca9be6b544170ca8568
Chris Rizzitello <sithlord48@gmail.com>
2022-07-27 14:24:19 -0400

    add Akao

---
+++ src/formats/Akao.h
@@ -0,0 +1,28 @@
+/****************************************************************************/
+//    copyright 2009 - 2022  Arzel Jérôme <myst6re@gmail.com>               //
+//                                                                          //
+//    This file is part of FF7tk                                            //
+//                                                                          //
+//    FF7tk is free software: you can redistribute it and/or modify         //
+//    it under the terms of the GNU General Public License as published by  //
+//    the Free Software Foundation, either version 3 of the License, or     //
+//    (at your option) any later version.                                   //
+//                                                                          //
+//    FF7tk is distributed in the hope that it will be useful,              //
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of        //
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         //
+//    GNU General Public License for more details.                          //
+/****************************************************************************/
+#pragma once
+
+#include <QtCore>
+#include <ff7tkformats_export.h>
+
+class FF7TKFORMATS_EXPORT Akao
+{
+public:
+    Akao();
+private:
+    quint16 _musicID, _length, _reverbMode;
+    QMap<quint8, QByteArray> _data;
+};
